81c49569c21470a4fe37091819a6cc372f52fabe
JorgeIba <jorgeluisibanezcanales@gmail.com>
2020-01-02 19:51:18 -0600

    Ver1.0

---
+++ Ping Pong.py
@@ -0,0 +1,156 @@
+#Ping Pong with Turtle - Python for Begginers by Jorge Ibanez
+#Inspired in youtube video: 
+#------------------------------------------------------------------------
+#Ping Pong con Turtle - Python para principiantes, hecho por Jorge Ibanez
+#Inspirado en el video de youtube:
+
+import turtle
+
+#Initialization
+ventana = turtle.Screen() #Creating and configuration of the window
+ventana.title("Ping Pong by Jorge Ibanez")
+ventana.bgcolor("black") 
+ventana.setup(width = 760, height = 640)
+ventana.delay(0)
+ScoreP1, ScoreP2 = 0, 0
+
+#Raqueta 1 - Paddle 1
+raqueta1 = turtle.Turtle()
+raqueta1.shape("square")
+raqueta1.penup()
+raqueta1.shapesize(stretch_wid = 4, stretch_len = 1) #Width = 80p and Length = 20p
+raqueta1.color("white")
+raqueta1.speed(0)
+raqueta1.goto(-360,0)
+
+#Raqueta 2 - Paddle 2
+raqueta2 = turtle.Turtle()
+raqueta2.shape("square")
+raqueta2.penup()
+raqueta2.shapesize(stretch_wid = 4, stretch_len = 1)
+raqueta2.color("white")
+raqueta2.speed(0)
+raqueta2.goto(360,0)
+
+#Pelota - Ball
+pelota = turtle.Turtle()
+pelota.shape("square")
+pelota.color("white")
+pelota.penup()
+pelota.speed(0)
+pelota.goto(0,0)
+pelota.dirX = 1
+pelota.dirY = 1
+
+#Letras y Puntuacion - Letters and Score
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.ht()
+def Draw():
+	global ScoreP1, ScoreP2, pen
+	pen.clear()
+	pen.goto(0,320) #To draw the midline
+	pen.pendown()
+	pen.goto(0,-320)
+	pen.penup()
+	pen.goto(0, 190)
+	pen.write("Player 1: {}    Player 2: {}".format(ScoreP1, ScoreP2), align="center", font=("Courier", 24, "normal") )
+Draw()
+
+#Configuracion del Teclado - Keyboard configuration
+def Raqueta1Up():
+	y = raqueta1.ycor()
+	VentanaHeight = ventana.window_height()
+	if y + 40 < VentanaHeight//2:
+		y+=40
+		x = raqueta1.xcor()		
+		raqueta1.goto(x,y)
+def Raqueta1Down():
+	y = raqueta1.ycor()
+	VentanaHeight = ventana.window_height()
+	if y - 40 > -VentanaHeight//2:
+		y-=40
+		x = raqueta1.xcor()		
+		raqueta1.goto(x,y)
+def Raqueta2Up():
+	y = raqueta2.ycor()
+	VentanaHeight = ventana.window_height()
+	if y + 40< VentanaHeight//2:
+		y+=40
+		x = raqueta2.xcor()		
+		raqueta2.goto(x,y)
+def Raqueta2Down():
+	y = raqueta2.ycor()
+	VentanaHeight = ventana.window_height()
+	if y - 40 > -VentanaHeight//2:
+		y-=40
+		x = raqueta2.xcor()		
+		raqueta2.goto(x,y)
+ventana.listen()
+ventana.onkeypress(Raqueta1Up, "w")
+ventana.onkeypress(Raqueta1Down, "s")
+ventana.onkeypress(Raqueta2Up, "Up")
+ventana.onkeypress(Raqueta2Down, "Down")
+
+#Colisiones - Collisions
+def ChoqueArriba(coordY):
+	global pelota
+	VentanaHeight = ventana.window_height()
+	if coordY>= VentanaHeight//2 - 10:
+		pelota.dirY = -pelota.dirY
+def ChoqueAbajo(coordY):
+	global pelota
+	VentanaHeight = ventana.window_height()
+	if coordY<= -VentanaHeight//2 + 10:
+		pelota.dirY = -pelota.dirY
+def ChoqueDerecha(coordX):
+	global pelota
+	VentanaWidth = ventana.window_width()
+	if coordX >= VentanaWidth//2 - 10:
+		pelota.dirX = -pelota.dirX
+		return True
+	return False
+
+def ChoqueIzquierda(coordX):
+	global pelota
+	VentanaWidth = ventana.window_width()
+	if coordX<= -VentanaWidth//2 + 10:
+		pelota.dirX = -pelota.dirX
+		return True
+	return False
+
+def ChoqueRaqueta1(coordX, coordY):
+	global pelota, raqueta1
+	if  (raqueta1.ycor() + 40) >= coordY-10 and  coordY + 10 >= (raqueta1.ycor() -40) and coordX - 10 <= raqueta1.xcor() + 10:
+		pelota.dirX = -pelota.dirX
+		if coordX - 10 < raqueta1.xcor() + 10:
+			pelota.setx( raqueta1.xcor() + 20 )
+			
+def ChoqueRaqueta2(coordX, coordY):
+	global pelota, raqueta2
+	if  (raqueta2.ycor() + 40) >= coordY-10 and  coordY + 10 >= (raqueta2.ycor() -40) and coordX + 10 >= raqueta2.xcor() - 10:
+		pelota.dirX = -pelota.dirX
+		if coordX + 10 >= raqueta2.xcor() - 10:
+			pelota.setx( raqueta2.xcor() - 20 )
+
+#Refreshing
+
+while True:
+	ventana.update()
+	#Movimiento Pelota - Ball movement
+	pelota.setx( pelota.xcor() + pelota.dirX)
+	pelota.sety( pelota.ycor() + pelota.dirY)
+	ChoqueArriba( pelota.ycor() )
+	ChoqueAbajo( pelota.ycor() )
+	if ChoqueDerecha( pelota.xcor() ): #Score Player 1
+		ScoreP1+=1
+		pelota.goto(0,0)
+		Draw()
+	if ChoqueIzquierda( pelota.xcor() ): #Score Player 2
+		ScoreP2+=1
+		pelota.goto(0,0)
+		Draw()
+	ChoqueRaqueta1( pelota.xcor(), pelota.ycor())
+	ChoqueRaqueta2( pelota.xcor(), pelota.ycor())
+
